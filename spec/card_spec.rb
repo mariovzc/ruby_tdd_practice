09921bb0011f41e6d683009595bd55c38f8a46e2
Mario Vizcaino <mario.vizcainos@gmail.com>
2021-04-28 21:21:18 -0500

    Let and custom error message example

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,14 +2,34 @@ require_relative '../files/Card'
 
 # collection of tests
 RSpec.describe Card do
-  it 'has a rank' do
-    card = Card.new('Ace', 'Spades')
+  # memoization item for each it 
+  # lazy loading item
+  let(:card) { Card.new('Ace', 'Spades') }
+  # let(:x) { 1 + 1 }
+  # let(:y) { x + 10 }
+  
+  # def card
+  #   Card.new('Ace', 'Spades')
+  # end
+
+  # before do
+  #   @card = Card.new('Ace', 'Spades')
+  # end
+
+
+  it 'has a rank and that rank can change' do
     expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
   end
 
   it 'has a suit' do
-    card = Card.new('Ace', 'Spades')
     expect(card.rank).to eq('Ace')
   end
 
+  # it 'has a custom error message' do
+  #   card.suit = 'Nonsense'
+  #   comparison = 'Spade'
+  #   expect(card.suit).to eq(comparison), "Hey, I Expected #{comparison} but i got #{card.suit} instead!"
+  # end
 end
