820a012845c2f1d521cecf820591c3cf27cbfad1
Mario Vizcaino <mario.vizcainos@gmail.com>
2021-06-17 18:43:10 -0500

    ADD: predicate_methods

---
+++ spec/predicate_methods_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe 'Predicate methods and predicate matchers' do
+  it 'can be tested with ruby methods' do
+    result = 16 / 2
+    expect(result.even?).to eq(true)
+  end
+  
+  it 'can be tested with predicate methods' do
+    # despues del be puede ir cualquier metodo
+    expect(16 / 2).to be_even
+    expect(15).to be_odd
+    expect(0).to be_zero
+    expect([]).to be_empty
+  end
+
+  describe 0 do
+    it { is_expected.to be_zero }
+  end
+end
